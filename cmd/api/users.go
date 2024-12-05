77e9f37dec511aa9132e8cf3b2d416b9f6268f3a
prashant <prashantbharatirajak@gmail.com>
2024-12-06 00:06:10 +0530

    implement email activation code

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.prashant.net/internal/data"
 	"greenlight.prashant.net/internal/validator"
@@ -51,9 +52,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	app.background(func() {
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+	app.background(func() {
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
--- go.mod
+++ go.mod
@@ -13,6 +13,4 @@ require (
 
 require github.com/lib/pq v1.10.9 //
 
-// 322 4 Dec
-
-
+// 337 5 Dec
